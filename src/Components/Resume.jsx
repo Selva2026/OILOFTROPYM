36dd57892016e0b09c4e60c2063199c3c5076efb
Muthupandi Thamaraiselvam <selvapandi2014@gmail.com>
2025-04-04 21:17:58 +0530

    Update Resume.jsx

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Resume = () => {
-  const resumePath = "/src/assets/MERN_CV_MP.pdf"; 
+  const resumePath = "./src/assets/MERN_CV_MP.pdf"; 
 
   return (
     <div className="flex flex-col items-center justify-center  p-4 bg-gray-100">
