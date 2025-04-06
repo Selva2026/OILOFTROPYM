a1ca0a74bf8ee7ed155ec67a63c718719e1251dd
Muthupandi Thamaraiselvam <selvapandi2014@gmail.com>
2025-04-06 07:57:08 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,38 +1,37 @@
 import React from 'react';
 import { TypeAnimation } from 'react-type-animation';
 
-
 const About = () => {
-    
   return (
-    <div className=' bg-[url(https://res.cloudinary.com/dn3jekp3r/image/upload/c_fill,w_1200,h_900,ar_4:3/v1743219370/my_r9aids.jpg)]  w-280 h-163' >
-     <div className='flex flex-col h-163 backdrop-brightness-50 '>
-        
-      <h1 className='text-white  mt-68 font-bold mx-4 text-6xl'>Muthpandi<br/> ThamaraiSelvam</h1>
-      
-      <TypeAnimation className=' text-gray-200 font-bold mx-6 mt-4 text-4xl'
-      sequence={[
-     
-        'I AM A FULL STACK DEVELOPER',
-        1000, 
-        'I AM A FRONT-END DEVELOPER',
-        1000,
-        'I AM A BACK-END DEVELOPER',
-        1000,
-        'I AM A DEVELOPER',
-        1000
-      ]}
-      wrapper="span"
-      speed={50}
-      style={{ fontSize: '2em', display: 'inline-block' }}
-      repeat={Infinity}
-    />
+    <div
+      className="w-full h-screen bg-[url('https://res.cloudinary.com/dn3jekp3r/image/upload/v1743219370/my_r9aids.jpg')] bg-cover bg-center md:bg-[center] bg-[position:right_top] md:bg-[position:center]"
+    >
+      {/* Dark overlay */}
+      <div className="flex flex-col h-full backdrop-brightness-50 justify-center md:items-start items-center text-center md:text-left px-4 md:px-12">
+        <h1 className="text-white font-bold text-5xl md:text-8xl leading-tight">
+          Muthupandi <br className="hidden md:block" /> ThamaraiSelvam
+        </h1>
 
+        <TypeAnimation
+          className="text-gray-200 font-bold mt-4 text-2xl md:text-4xl"
+          sequence={[
+            'I AM A FULL STACK DEVELOPER',
+            1000,
+            'I AM A FRONT-END DEVELOPER',
+            1000,
+            'I AM A BACK-END DEVELOPER',
+            1000,
+            'I AM A DEVELOPER',
+            1000,
+          ]}
+          wrapper="span"
+          speed={50}
+          style={{ display: 'inline-block' }}
+          repeat={Infinity}
+        />
       </div>
-     
-    
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
